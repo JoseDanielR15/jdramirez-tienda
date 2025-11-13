e97df33fe09c2651e4b9a023830a4996119f4cc9
JoseDanielR15 <josedanielr163@gmail.com>
2025-11-13 17:34:35 -0600

    Semana 10
    En esta semana no tuvimos clase pero tuvimos de tarea agregar muchas clases java en los paquetes de com.tienda, de domain, services y repository.
    
    Tambien se edito una pequeña parte en fragmentos de general.
    
    Por el momento tengo un error de compilacion pero ya estoy preguntandole al profe como arreglar el error.

--- jdramirez-tienda/src/main/java/com/tienda/ProjectConfig.java
+++ jdramirez-tienda/src/main/java/com/tienda/ProjectConfig.java
@@ -120,34 +120,48 @@ public class ProjectConfig implements WebMvcConfigurer {
         "/facturar/carrito"
     };
 
+    @Autowired
+    private RutaService rutaService;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(request -> request
-                .requestMatchers(PUBLIC_URLS).permitAll()
-                .requestMatchers(ADMIN_URLS).hasRole("ADMIN")
-                .requestMatchers(ADMIN_OR_VENDEDOR_URLS).hasAnyRole("ADMIN", "VENDEDOR")
-                .requestMatchers(USUARIO_URLS).hasRole("USUARIO")
-                .anyRequest().authenticated()
-        ).formLogin(form -> form // Configuración de formulario de login
-                .loginPage("/login")
-                .loginProcessingUrl("/login")
-                .defaultSuccessUrl("/", true)
-                .failureUrl("/login?error=true")
-                .permitAll()
-        ).logout(logout -> logout // Configuración de logout
-                .logoutUrl("/logout")
-                .logoutSuccessUrl("/login?logout=true")
-                .invalidateHttpSession(true)
-                .deleteCookies("JSESSIONID")
-                .permitAll()
-        ).exceptionHandling(exceptions -> exceptions // Manejo de excepciones
-                .accessDeniedPage("/acceso_denegado")
-        ).sessionManagement(session -> session // Configuración de sesiones
-                .maximumSessions(1)
-                .maxSessionsPreventsLogin(false)
-        );
-        return http.build();
-    }
+    var rutas = rutaService.getRutas();
+    http.authorizeHttpRequests(requests -> {
+        for (Ruta ruta : rutas) {
+            if (ruta.isRequiereRol()) {
+                requests.requestMatchers(ruta.getRuta()).hasRole(ruta.getRol().getRol());
+            } else {
+                requests.requestMatchers(ruta.getRuta()).permitAll();
+            }
+        }
+        requests.anyRequest().authenticated();
+    });
+
+    http.formLogin(form -> form // Configuración de formulario de login
+            .loginPage("/login")
+            .loginProcessingUrl("/login")
+            .defaultSuccessUrl("/", true)
+            .failureUrl("/login?error=true")
+            .permitAll()
+    )
+    .logout(logout -> logout // Configuración de logout
+            .logoutUrl("/logout")
+            .logoutSuccessUrl("/login?logout=true")
+            .invalidateHttpSession(true)
+            .deleteCookies("JSESSIONID")
+            .permitAll()
+    )
+    .exceptionHandling(exceptions -> exceptions // Manejo de excepciones
+            .accessDeniedPage("/acceso_denegado")
+    )
+    .sessionManagement(session -> session // Configuración de sesiones
+            .maximumSessions(1)
+            .maxSessionsPreventsLogin(false)
+    );
+
+    return http.build();
+}
+
 
      @Bean
     public PasswordEncoder passwordEncoder() {
