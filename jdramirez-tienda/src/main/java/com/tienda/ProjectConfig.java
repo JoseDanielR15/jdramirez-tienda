7f14df2354ee5c8692f3edde5df93d7e7e45370b
JoseDanielR15 <josedanielr163@gmail.com>
2025-12-04 17:35:40 -0600

    Semana 13
    Arreglo del error en rojectConfig, ya funciona bien y habre la pagina

--- jdramirez-tienda/src/main/java/com/tienda/ProjectConfig.java
+++ jdramirez-tienda/src/main/java/com/tienda/ProjectConfig.java
@@ -1,26 +1,8 @@
 package com.tienda;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.storage.Storage;
-import com.google.cloud.storage.StorageOptions;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.Locale;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -29,10 +11,10 @@ import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.templatemode.TemplateMode;
-
+ 
 @Configuration
 public class ProjectConfig implements WebMvcConfigurer {
-
+ 
     /* Los siguiente métodos son para implementar el tema de seguridad dentro del proyecto */
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
@@ -43,7 +25,7 @@ public class ProjectConfig implements WebMvcConfigurer {
         registry.addViewController("/login").setViewName("login");
         registry.addViewController("/registro/nuevo").setViewName("/registro/nuevo");
     }
-
+ 
     /* El siguiente método se utilizar para publicar en la nube, independientemente  */
     @Bean
     public SpringResourceTemplateResolver templateResolver_0() {
@@ -55,7 +37,7 @@ public class ProjectConfig implements WebMvcConfigurer {
         resolver.setCheckExistence(true);
         return resolver;
     }
-
+ 
     @Bean
     public LocaleResolver localeResolver() {
         var slr = new SessionLocaleResolver();
@@ -64,132 +46,16 @@ public class ProjectConfig implements WebMvcConfigurer {
         slr.setTimeZoneAttributeName("session.current.timezone");
         return slr;
     }
-
+ 
     @Bean
     public LocaleChangeInterceptor localeChangeInterceptor() {
         var lci = new LocaleChangeInterceptor();
         lci.setParamName("lang");
         return lci;
     }
-
+ 
     @Override
     public void addInterceptors(InterceptorRegistry registro) {
         registro.addInterceptor(localeChangeInterceptor());
-    }
-
-    //Bean para poder acceder a los messages.properties en código...
-    @Bean("messageSource")
-    public MessageSource messageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasenames("messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        return messageSource;
-    }
-   @Value("${firebase.json.path}")
-    private String jsonPath;
-   
-    @Value("${firebase.json.file}")
-    private String jsonFile;
-    
-    @Bean
-    public Storage storage() throws IOException {
-        ClassPathResource resource = new ClassPathResource(jsonPath + File.separator + jsonFile);
-        try (InputStream inputStream = resource.getInputStream()) {
-            GoogleCredentials credentials = GoogleCredentials.fromStream(inputStream);
-            return StorageOptions.newBuilder().setCredentials(credentials).build().getService();
-        }
-    }
-    
-    //Código agregado en semana 9
-     public static final String[] PUBLIC_URLS = {
-        "/", "/index", "/fav/**", "/carrito/**", "/consultas/**", "/registro/**",
-        "/js/**", "/webjars/**", "/login", "/acceso_denegado"
-    };
-
-    public static final String[] ADMIN_URLS = {
-        "/producto/nuevo", "/producto/guardar", "/producto/modificar/**", "/producto/eliminar/**",
-        "/categoria/nuevo", "/categoria/guardar", "/categoria/modificar/**", "/categoria/eliminar/**",
-        "/usuario/nuevo", "/usuario/guardar", "/usuario/modificar/**", "/usuario/eliminar/**"
-    };
-
-    public static final String[] ADMIN_OR_VENDEDOR_URLS = {
-        "/producto/listado", "/categoria/listado", "/usuario/listado"
-    };
-
-    public static final String[] USUARIO_URLS = {
-        "/facturar/carrito"
-    };
-
-    @Autowired
-    private RutaService rutaService;
-
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    var rutas = rutaService.getRutas();
-    http.authorizeHttpRequests(requests -> {
-        for (Ruta ruta : rutas) {
-            if (ruta.isRequiereRol()) {
-                requests.requestMatchers(ruta.getRuta()).hasRole(ruta.getRol().getRol());
-            } else {
-                requests.requestMatchers(ruta.getRuta()).permitAll();
-            }
-        }
-        requests.anyRequest().authenticated();
-    });
-
-    http.formLogin(form -> form // Configuración de formulario de login
-            .loginPage("/login")
-            .loginProcessingUrl("/login")
-            .defaultSuccessUrl("/", true)
-            .failureUrl("/login?error=true")
-            .permitAll()
-    )
-    .logout(logout -> logout // Configuración de logout
-            .logoutUrl("/logout")
-            .logoutSuccessUrl("/login?logout=true")
-            .invalidateHttpSession(true)
-            .deleteCookies("JSESSIONID")
-            .permitAll()
-    )
-    .exceptionHandling(exceptions -> exceptions // Manejo de excepciones
-            .accessDeniedPage("/acceso_denegado")
-    )
-    .sessionManagement(session -> session // Configuración de sesiones
-            .maximumSessions(1)
-            .maxSessionsPreventsLogin(false)
-    );
-
-    return http.build();
-}
-
-
-     @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    //Este método será reemplazado la siguiente semana
-    @Bean
-    public UserDetailsService users(PasswordEncoder passwordEncoder) {
-        UserDetails admin = User.builder()
-                .username("juan")
-                .password(passwordEncoder.encode("123"))
-                .roles("ADMIN")
-                .build();
-
-        UserDetails sales = User.builder()
-                .username("rebeca")
-                .password(passwordEncoder.encode("456"))
-                .roles("VENDEDOR")
-                .build();
-
-        UserDetails user = User.builder()
-                .username("pedro")
-                .password(passwordEncoder.encode("789"))
-                .roles("USUARIO") // Consistent con tu configuración
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, sales, user);
-    }
-
+    }   
 }
